41f85de8507cacf3290c6d6d70839efe1eaa8d15
linsalrob <raedwards@gmail.com>
2025-08-13 13:45:34 +0930

    importing colors

--- src/simple_ncbi_taxonomy/__init__.py
+++ src/simple_ncbi_taxonomy/__init__.py
@@ -3,3 +3,4 @@
 from .config import get_db_dir
 from .load_data import read_taxa, read_nodes, extended_names, read_divisions, read_names, read_gi_tax_id, read_tax_id_gi, load_ncbi_taxonomy
 from .taxonomy import TaxonName, TaxonDivision, TaxonNode, NoNameFoundError 
+from .bcolors import bcolors
